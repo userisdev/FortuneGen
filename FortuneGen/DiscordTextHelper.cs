db54e9d4267882373157c977be63c64a061b3cde
Kento Ooiwa <lunatic.heros@gmail.com>
2024-03-14 12:58:44 +0900

    first commit.

---
+++ FortuneGen/DiscordTextHelper.cs
@@ -0,0 +1,68 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+/// <summary> DiscordTextHelper class. </summary>
+internal static partial class DiscordTextHelper
+{
+    /// <summary> Divides the specified text. </summary>
+    /// <param name="text"> The text. </param>
+    /// <returns> </returns>
+    public static IEnumerable<string> Divide(this string text)
+    {
+        int lastIndex = 0;
+        IEnumerable<Match> emojiMatches = EmojiRegex().Matches(text).Cast<Match>();
+        foreach (Match match in emojiMatches)
+        {
+            // 直前の絵文字の後から現在の絵文字の前までの部分をクラスタとして処理
+            string cluster = text[lastIndex..match.Index];
+            foreach (string s in SplitTextElement(cluster))
+            {
+                yield return s;
+            }
+
+            yield return match.Value;
+
+            // 最後にマッチしたインデックスを更新
+            lastIndex = match.Index + match.Length;
+        }
+
+        // 最後の絵文字の後から文字列の最後までの部分をクラスタとして処理
+        string lastCluster = text[lastIndex..];
+        foreach (string s in SplitTextElement(lastCluster))
+        {
+            yield return s;
+        }
+    }
+
+    /// <summary> Randoms the specified text. </summary>
+    /// <param name="text"> The text. </param>
+    /// <returns> </returns>
+    public static string Random(this string text)
+    {
+        return string.Join(string.Empty, text.Divide().Select(e => (text: e, guid: Guid.NewGuid())).OrderBy(e => e.guid).Select(e => e.text));
+    }
+
+    /// <summary> Reverses the specified text. </summary>
+    /// <param name="text"> The text. </param>
+    /// <returns> </returns>
+    public static string Reverse(this string text)
+    {
+        return string.Join(string.Empty, text.Divide().Reverse());
+    }
+
+    /// <summary> Emojis the regex. </summary>
+    /// <returns> </returns>
+    [GeneratedRegex(@"<(a)?:.*?:(\d+)>")]
+    private static partial Regex EmojiRegex();
+
+    /// <summary> Splits the text element. </summary>
+    /// <param name="input"> The input. </param>
+    /// <returns> </returns>
+    private static IEnumerable<string> SplitTextElement(string input)
+    {
+        TextElementEnumerator enumerator = StringInfo.GetTextElementEnumerator(input);
+        while (enumerator.MoveNext())
+        {
+            yield return enumerator.GetTextElement();
+        }
+    }
+}
---
+++ FortuneGen/Program.cs
@@ -0,0 +1,77 @@
+﻿using SkiaSharp;
+using System;
+FileInfo Generate(string text, SKColor color, SKColor bg)
+{
+    var width = 100;
+    var height = 170;
+
+    var borderWidth = 20;
+    var borderMargin = 1;
+
+    var textDrawWidth = width - (borderWidth + borderMargin) * 2;
+    var textDrawHeight = height - (borderWidth + borderMargin) * 2;
+
+    string fontPath = Path.Combine(Environment.CurrentDirectory, "玉ねぎ楷書激無料版v7改.ttf");
+
+
+    using SKSurface surface = SKSurface.Create(new SKImageInfo(width, height));
+    using SKCanvas canvas = surface.Canvas;
+
+    canvas.Clear(bg);
+
+    using SKPaint rectPaint = new()
+    {
+        Style = SKPaintStyle.Stroke,
+        Color = color,
+        StrokeWidth = borderWidth,
+    };
+
+    SKRect borderRect = new(0, 0, width, height);
+    canvas.DrawRect(borderRect, rectPaint);
+
+    var items = text.Divide().ToArray();
+
+    using SKPaint textPaint = new()
+    {
+        Color = color,
+        TextSize = textDrawHeight / items.Length,
+        TextAlign = SKTextAlign.Center,
+        Typeface = SKTypeface.FromFile(fontPath),
+        SubpixelText = true
+    };
+
+    while(textDrawWidth < items.Select(textPaint.MeasureText).Max())
+    {
+        textPaint.TextSize -= 1;
+    }
+
+    var charHeight = textPaint.FontMetrics.Descent - textPaint.FontMetrics.Ascent;
+    var textHeight = charHeight * items.Length;
+    var yOffset = (height - textHeight)/2;
+
+    foreach (var (item, index) in items.Select((e, i) => (e, i)))
+    {
+        float drawSize = textPaint.MeasureText(text);
+        canvas.DrawText(item, 1.0f*width/2, yOffset +index*charHeight - textPaint.FontMetrics.Ascent, textPaint);
+    }
+
+    using SKImage img = surface.Snapshot();
+    using SKData tmp = img.Encode();
+
+    var savePath = Path.Combine(Environment.CurrentDirectory, $"{text}.png");
+    if (File.Exists(savePath))
+    {
+        File.Delete(savePath);
+    }
+
+    using var fs = File.Create(savePath);
+    tmp.SaveTo(fs);
+
+    return new FileInfo(savePath);
+}
+
+var items = new[] { "大吉", "吉", "中吉", "小吉", "末吉", "凶", "大凶","こんにちは" };
+foreach (var item in items)
+{
+    Generate(item, SKColors.Red, SKColors.White);
+}
